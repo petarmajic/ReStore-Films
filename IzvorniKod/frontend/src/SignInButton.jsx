67020311e49504353b34acb998dcf892b32112fe
zvonimirkilic <zvonimir.kilic@fer.unizg.hr>
2024-11-06 15:34:22 +0100

    frontend/ Log in i redirect na /home

--- IzvorniKod/frontend/src/SignInButton.jsx
+++ IzvorniKod/frontend/src/SignInButton.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "./authConfig";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
 
 /**
  * Renders a drop down button with child buttons for logging in with a popup or redirect
@@ -13,29 +12,15 @@ export const SignInButton = () => {
   const { instance } = useMsal();
 
   const handleLogin = (loginType) => {
-    if (loginType === "popup") {
-      instance.loginPopup(loginRequest).catch((e) => {
-        console.log(e);
-      });
-    } else if (loginType === "redirect") {
       instance.loginRedirect(loginRequest).catch((e) => {
         console.log(e);
       });
     }
-  };
   return (
-    <DropdownButton
+    <Button  as="button" onClick={() => handleLogin("redirect")}
       variant="secondary"
-      className="ml-auto"
-      drop="start"
-      title="Sign In"
-    >
-      <Dropdown.Item as="button" onClick={() => handleLogin("popup")}>
-        Sign in using Popup
-      </Dropdown.Item>
-      <Dropdown.Item as="button" onClick={() => handleLogin("redirect")}>
-        Sign in using Redirect
-      </Dropdown.Item>
-    </DropdownButton>
+      className="ml-auto">
+      Sign in
+    </Button>
   );
 };
