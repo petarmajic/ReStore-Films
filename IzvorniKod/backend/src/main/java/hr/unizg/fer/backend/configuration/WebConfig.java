e062faf540a045d26c99434fa0e32fde1651b112
Matej Šimek <matej.simek@fer.unizg.hr>
2024-12-19 15:09:12 +0100

    backend/ config updatead

--- IzvorniKod/backend/src/main/java/hr/unizg/fer/backend/configuration/WebConfig.java
+++ IzvorniKod/backend/src/main/java/hr/unizg/fer/backend/configuration/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173/",
-                                "https://restore-films-backend.onrender.com")
+                                "https://restore-films-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
