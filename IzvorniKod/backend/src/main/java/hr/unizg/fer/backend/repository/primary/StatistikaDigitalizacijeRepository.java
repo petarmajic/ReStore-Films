1262862ff3ab96351f18ca95763cc55a1eed47d4
Petar Majić <petar.majic@fer.unizg.hr>
2025-01-09 19:15:56 +0100

    Izbrisana statistika digitalizacije

--- IzvorniKod/backend/src/main/java/hr/unizg/fer/backend/repository/primary/StatistikaDigitalizacijeRepository.java
+++
@@ -1,9 +0,0 @@
-package hr.unizg.fer.backend.repository.primary;
-
-import hr.unizg.fer.backend.model.primary.FilmskaTraka;
-import hr.unizg.fer.backend.model.primary.StatistikaDigitalizacije;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatistikaDigitalizacijeRepository extends JpaRepository<StatistikaDigitalizacije, Long> { }
